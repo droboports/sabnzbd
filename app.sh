f9b704809af73d2ef34b4cf59bbf1ad647c62ed5
Ricardo Padilha <ricardospadilha@gmail.com>
2015-07-08 17:41:20 -0700

    Updated sabnzbd to 0.7.20, pyopenssl 0.15.1, unrar 5.2.7, new build and app scripts.

--- app.sh
+++ app.sh
@@ -1,49 +1,36 @@
 ### UNRAR ###
 _build_unrar() {
-local VERSION="5.1.7"
+local VERSION="5.2.7"
 local FOLDER="unrar"
-local FILE="unrar.tgz"
-local URL="https://github.com/droboports/unrar/releases/download/v${VERSION}/${FILE}"
+local FILE="unrarsrc-${VERSION}.tar.gz"
+local URL="http://www.rarlab.com/rar/${FILE}"
 
-_download_app "${FILE}" "${URL}" "${FOLDER}"
+_download_tgz "${FILE}" "${URL}" "${FOLDER}"
+pushd target/"${FOLDER}"
+mv makefile Makefile
+make CXX="${CXX}" CXXFLAGS="${CFLAGS} -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE" STRIP="${STRIP}" LDFLAGS="${LDFLAGS} -pthread"
+make install DESTDIR="${DEST}"
 mkdir -p "${DEST}/libexec"
-cp -v "target/${FOLDER}/bin"/* "${DEST}/libexec/"
+mv "${DEST}/bin/unrar" "${DEST}/libexec/"
+popd
 }
 
-### PAR2CMDLINE ###
+### PAR2 ###
 _build_par2() {
 local VERSION="0.4"
-local FOLDER="par2"
-local FILE="par2.tgz"
-local URL="https://github.com/droboports/par2cmdline/releases/download/v${VERSION}/${FILE}"
+local FOLDER="par2cmdline-${VERSION}"
+local FILE="${FOLDER}.tar.gz"
+local URL="http://sourceforge.net/projects/parchive/files/par2cmdline/${VERSION}/${FILE}"
 
-_download_app "${FILE}" "${URL}" "${FOLDER}"
+_download_tgz "${FILE}" "${URL}" "${FOLDER}"
+patch "target/${FOLDER}/reedsolomon.cpp" src/reedsolomon.cpp.patch
+pushd "target/${FOLDER}"
+./configure --host="${HOST}" --prefix="${DEST}"
+make
+make install
 mkdir -p "${DEST}/libexec"
-cp -v "target/${FOLDER}/bin"/* "${DEST}/libexec/"
-}
-
-### CHEETAH ###
-_build_cheetah() {
-local VERSION="2.4.4"
-local FILE="Cheetah-${VERSION}-py2.7-linux-armv7l.egg"
-local URL="https://github.com/droboports/python-cheetah/releases/download/v${VERSION}/${FILE}"
-local XPYTHON=~/xtools/python2/${DROBO}
-
-_download_file "${FILE}" "${URL}"
-mkdir -p "${DEST}/lib/python2.7/site-packages"
-_PYTHON_HOST_PLATFORM="linux-armv7l" PYTHONPATH="${DEST}/lib/python2.7/site-packages" ${XPYTHON}/bin/easy_install --prefix="${DEST}" --always-copy "download/${FILE}"
-}
-
-### PYOPENSSL ###
-_build_pyopenssl() {
-local VERSION="0.13"
-local FILE="pyOpenSSL-${VERSION}-py2.7-linux-armv7l.egg"
-local URL="https://github.com/droboports/python-pyopenssl/releases/download/v${VERSION}/${FILE}"
-local XPYTHON=~/xtools/python2/${DROBO}
-
-_download_file "${FILE}" "${URL}"
-mkdir -p "${DEST}/lib/python2.7/site-packages"
-_PYTHON_HOST_PLATFORM="linux-armv7l" PYTHONPATH="${DEST}/lib/python2.7/site-packages" ${XPYTHON}/bin/easy_install --prefix="${DEST}" --always-copy "download/${FILE}"
+mv "${DEST}/bin/par2"* "${DEST}/libexec/"
+popd
 }
 
 ### YENC ###
@@ -51,41 +38,98 @@ _build_yenc() {
 local VERSION="0.4.0"
 local FILE="yenc-${VERSION}-py2.7-linux-armv7l.egg"
 local URL="https://github.com/droboports/python-yenc/releases/download/v${VERSION}/${FILE}"
-local XPYTHON=~/xtools/python2/${DROBO}
+local XPYTHON="${HOME}/xtools/python2/${DROBO}"
+export QEMU_LD_PREFIX="${TOOLCHAIN}/${HOST}/libc"
 
 _download_file "${FILE}" "${URL}"
 mkdir -p "${DEST}/lib/python2.7/site-packages"
-_PYTHON_HOST_PLATFORM="linux-armv7l" PYTHONPATH="${DEST}/lib/python2.7/site-packages" ${XPYTHON}/bin/easy_install --prefix="${DEST}" --always-copy "download/${FILE}"
+PYTHONPATH="${DEST}/lib/python2.7/site-packages" \
+  "${XPYTHON}/bin/easy_install" --prefix="${DEST}" --always-copy "download/${FILE}"
+}
+
+### CHEETAH ###
+_build_cheetah() {
+# also installs Markdown
+local VERSION="2.4.4"
+local FILE="Cheetah-${VERSION}-py2.7-linux-armv7l.egg"
+local URL="https://github.com/droboports/python-cheetah/releases/download/v${VERSION}/${FILE}"
+local XPYTHON="${HOME}/xtools/python2/${DROBO}"
+export QEMU_LD_PREFIX="${TOOLCHAIN}/${HOST}/libc"
+
+_download_file "${FILE}" "${URL}"
+mkdir -p "${DEST}/lib/python2.7/site-packages"
+PYTHONPATH="${DEST}/lib/python2.7/site-packages" \
+  "${XPYTHON}/bin/easy_install" --prefix="${DEST}" --always-copy "download/${FILE}"
+}
+
+### CFFI ###
+_build_cffi() {
+# also installs pycparser
+local VERSION="1.1.2"
+local FILE="cffi-${VERSION}-py2.7-linux-armv7l.egg"
+local URL="https://github.com/droboports/python-cffi/releases/download/v${VERSION}/${FILE}"
+local XPYTHON="${HOME}/xtools/python2/${DROBO}"
+export QEMU_LD_PREFIX="${TOOLCHAIN}/${HOST}/libc"
+
+_download_file "${FILE}" "${URL}"
+mkdir -p "${DEST}/lib/python2.7/site-packages"
+PYTHONPATH="${DEST}/lib/python2.7/site-packages" \
+  "${XPYTHON}/bin/easy_install" --prefix="${DEST}" --always-copy "download/${FILE}"
+}
+
+### CRYPTOGRAPHY ###
+_build_cryptography() {
+# also installs ipaddress enum34 six pyasn1 idna
+# depends on cffi
+local VERSION="0.9.1"
+local FILE="cryptography-${VERSION}-py2.7-linux-armv7l.egg"
+local URL="https://github.com/droboports/python-cryptography/releases/download/v${VERSION}/${FILE}"
+local XPYTHON="${HOME}/xtools/python2/${DROBO}"
+export QEMU_LD_PREFIX="${TOOLCHAIN}/${HOST}/libc"
+
+_download_file "${FILE}" "${URL}"
+mkdir -p "${DEST}/lib/python2.7/site-packages"
+PYTHONPATH="${DEST}/lib/python2.7/site-packages" \
+  "${XPYTHON}/bin/easy_install" --prefix="${DEST}" --always-copy "download/${FILE}"
+}
+
+### PYOPENSSL ###
+_build_pyopenssl() {
+# depends on cryptography
+local VERSION="0.15.1"
+local FILE="pyOpenSSL-${VERSION}-py2.7.egg"
+local URL="https://github.com/droboports/python-pyopenssl/releases/download/v${VERSION}/${FILE}"
+local XPYTHON="${HOME}/xtools/python2/${DROBO}"
+export QEMU_LD_PREFIX="${TOOLCHAIN}/${HOST}/libc"
+
+_download_file "${FILE}" "${URL}"
+mkdir -p "${DEST}/lib/python2.7/site-packages"
+PYTHONPATH="${DEST}/lib/python2.7/site-packages" \
+  "${XPYTHON}/bin/easy_install" --prefix="${DEST}" --always-copy "download/${FILE}"
 }
 
 ### SABNZBD ###
 _build_sabnzbd() {
-local BRANCH="0.7.x"
-local FOLDER="app"
-local URL="https://github.com/sabnzbd/sabnzbd.git"
+local VERSION="0.7.20"
+local FOLDER="sabnzbd-${VERSION}"
+local FILE="${VERSION}.tar.gz"
+local URL="https://github.com/sabnzbd/sabnzbd/archive/${FILE}"
 
-_download_git "${BRANCH}" "${FOLDER}" "${URL}"
+_download_tgz "${FILE}" "${URL}" "${FOLDER}"
 rm -fr "target/${FOLDER}/win" "target/${FOLDER}/osx"
 mkdir -p "${DEST}/app"
 cp -avR "target/${FOLDER}"/* "${DEST}/app/"
-
-#local VERSION="0.7.18"
-#local FOLDER="SABnzbd-${VERSION}"
-#local FILE="${FOLDER}-src.tar.gz"
-#local URL="http://sourceforge.net/projects/sabnzbdplus/files/sabnzbdplus/${VERSION}/${FILE}"
-
-#_download_tgz "${FILE}" "${URL}" "${FOLDER}"
-#mkdir -p "${DEST}/app"
-#cp -avR "target/${FOLDER}"/* "${DEST}/app/"
 }
 
 ### BUILD ###
 _build() {
   _build_unrar
   _build_par2
-  _build_cheetah
-  _build_pyopenssl
   _build_yenc
+  _build_cheetah
+  _build_cffi
+  _build_cryptography
+  _build_pyopenssl
   _build_sabnzbd
   _package
 }
--- src/dest/service.sh
+++ src/dest/service.sh
@@ -5,90 +5,48 @@
 # import DroboApps framework functions
 . /etc/service.subr
 
-# DroboApp framework version
-framework_version="2.0"
-
-# app description
+framework_version="2.1"
 name="sabnzbd"
-version="0.7.19"
+version="0.7.20"
 description="Usenet downloader"
+depends="python2"
+webui=":8081/"
 
-# framework-mandated variables
-pidfile="/tmp/DroboApps/${name}/pid.txt"
-logfile="/tmp/DroboApps/${name}/log.txt"
-statusfile="/tmp/DroboApps/${name}/status.txt"
-errorfile="/tmp/DroboApps/${name}/error.txt"
-
-# app-specific variables
-prog_dir=$(dirname $(readlink -fn ${0}))
-python="${DROBOAPPS_DIR}/python2/bin/python"
+prog_dir="$(dirname "$(realpath "${0}")")"
+daemon="${DROBOAPPS_DIR}/python2/bin/python"
 conffile="${prog_dir}/data/sabnzbd.ini"
+tmp_dir="/tmp/DroboApps/${name}"
+pidfile="${tmp_dir}/pid.txt"
+logfile="${tmp_dir}/log.txt"
+statusfile="${tmp_dir}/status.txt"
+errorfile="${tmp_dir}/error.txt"
+nicelevel=19
 
-# script hardening
-set -o errexit  # exit on uncaught error code
-set -o nounset  # exit on unset variable
-set -o pipefail # propagate last error code on pipe
-
-# ensure log folder exists
-logfolder="$(dirname ${logfile})"
-if [[ ! -d "${logfolder}" ]]; then mkdir -p "${logfolder}"; fi
-
-# redirect all output to logfile
-exec 3>&1 1>> "${logfile}" 2>&1
-
-# log current date, time, and invocation parameters
-echo $(date +"%Y-%m-%d %H-%M-%S"): ${0} ${@}
-
-# enable script tracing
-set -o xtrace
-
-# _is_running
-# args: path to pid file
-# returns: 0 if pid is running, 1 if not running or if pidfile does not exist.
-_is_running() {
-  /sbin/start-stop-daemon -K -s 0 -x "${python}" -p "${pidfile}" -q
-}
+# backwards compatibility
+if [ -z "${FRAMEWORK_VERSION:-}" ]; then
+  framework_version="2.0"
+  . "${prog_dir}/libexec/service.subr"
+fi
 
 start() {
   local PORT=""
-  [[ ! -f "${conffile}" ]] && PORT=":8081"
-  rm -f "${pidfile}"
-  PATH="${prog_dir}/libexec:${DROBOAPPS_DIR}/git/bin:${PATH}" PYTHONPATH="${prog_dir}/lib/python2.7/site-packages" "${python}" "${prog_dir}/app/SABnzbd.py" --server 0.0.0.0${PORT} --config-file "${conffile}" --pidfile "${pidfile}" --browser 0 --daemon
+  if [ ! -f "${conffile}" ]; then PORT=":8081"; fi
+  PATH="${prog_dir}/libexec:${DROBOAPPS_DIR}/git/bin:${PATH}" \
+  PYTHONPATH="${prog_dir}/lib/python2.7/site-packages" \
+  "${daemon}" "${prog_dir}/app/SABnzbd.py" --server 0.0.0.0${PORT} --config-file "${conffile}" --pidfile "${pidfile}" --browser 0 --daemon
+  sleep 1
+  renice "${nicelevel}" $(cat "${pidfile}")
 }
 
-_service_start() {
-  set +e
-  set +u
-  if _is_running "${pidfile}"; then
-    echo ${name} is already running >&3
-    return 1
-  fi
-  start_service
-  set -u
-  set -e
-}
+# boilerplate
+if [ ! -d "${tmp_dir}" ]; then mkdir -p "${tmp_dir}"; fi
+exec 3>&1 4>&2 1>> "${logfile}" 2>&1
+STDOUT=">&3"
+STDERR=">&4"
+echo "$(date +"%Y-%m-%d %H-%M-%S"):" "${0}" "${@}"
+set -o errexit  # exit on uncaught error code
+set -o nounset  # exit on unset variable
+set -o pipefail # propagate last error code on pipe
+set -o xtrace   # enable script tracing
 
-_service_stop() {
-  /sbin/start-stop-daemon -K -x "${python}" -p "${pidfile}" -v || echo "${name} is not running" >&3
-}
-
-_service_restart() {
-  service_stop
-  sleep 3
-  service_start
-}
-
-_service_status() {
-  status >&3
-}
-
-_service_help() {
-  echo "Usage: $0 [start|stop|restart|status]" >&3
-  set +e
-  exit 1
-}
-
-case "${1:-}" in
-  start|stop|restart|status) _service_${1} ;;
-  *) _service_help ;;
-esac
+main "${@}"
